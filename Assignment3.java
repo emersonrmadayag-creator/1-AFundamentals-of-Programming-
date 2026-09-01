b9be0d3351a232a5d3e05dff06d8677bf932a2a0
emersonrmadayag-creator <emersonrmadayag@gmail.com>
2026-09-02 05:18:00 +0800

    Add files via upload

---
+++ Assignment3.java
@@ -0,0 +1,18 @@
+public class Assignment3
+{
+	public static void main (String[] args) { //starting point of the program
+		String firstname = "John Emerson" ; //declare a string variable named firstname
+		String lastname = "Madayag" ; //declare a string variable named lastname
+		String address = "Anonas, Urdaneta City, Pangasinan" ; // declare a string variable named address
+		String mobilenumber ="09063029136"; // declare a string variable named mobilenumber
+		int age = 18; // declare an int named age
+		float gwa = 96f ; // declare a float named gwa
+		
+		System.out.println("Firstname : " + firstname) ; // display the value of firstname
+		System.out.println("Lastname : " + lastname) ; ; // display the value of lastname
+		System.out.println("Address : " + address) ; ; // display the value of address
+		System.out.println("Mobile Number :" +  mobilenumber) ; ; // display the value of mobilenumber
+		System.out.println(" Age :" + age) ; ; // display the value of age
+		System.out.println("GWA :" + gwa) ; ; // display the value of gwa
+ 	}
+ }
