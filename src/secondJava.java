94f20f506b8e7af012d83858a4904161eebbe725
John Emerson R. Madayag <emersonrmadayag@gmail.com>
2026-09-23 07:58:44 +0800

    Add Java programming files

---
+++ src/secondJava.java
@@ -0,0 +1,16 @@
+public class secondJava {
+    public static void main(String[] args){
+
+        String name = "John Emerson R. Madayag";
+        String address = "#273 Anonas East, Urdaneta City, Pangasinan";
+        String contactNumber = "09063029136";
+        int age = 18;
+        String crush = "None";
+
+                System.out.println("Name : " + name);
+                System.out.println("Address : " + address);
+                System.out.println("Contact Number : " + contactNumber);
+                System.out.println("Age : " + age);
+                System.out.println("Crush : " + crush);
+    }
+}
